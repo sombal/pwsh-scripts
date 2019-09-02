01cfe1f7e99e17a952c9a83969c6eb6f41ad5c02
Balazs Somogyi <balazs.somogyi@t-systems.com>
2019-09-02 16:11:13 +0200

    add stuff

--- magpi_bulk_dl.ps1
+++ magpi_bulk_dl.ps1
@@ -4,13 +4,15 @@ $magpiIssues = Invoke-WebRequest -UseBasicParsing -Uri $prefix -Method GET
 $fileList = $magpiIssues.Links | ForEach-Object {$_.href}
 $dest = "$($env:userprofile)\Desktop\MagPi\"
 $timing = Measure-Command {
+    Write-Host "Downloading files" -NoNewline
     ForEach ($magFile in $fileList) {
         if ($magFile -like "*.pdf") {
             $url = $prefix+$magFile
-            $destfile = $dest+$magFile                
+            $destfile = $dest+$magFile
             (New-Object System.Net.WebClient).DownloadFile("$url","$destfile")
             Write-Host "." -NoNewline
         }
     }
 }
-$timing
+$elapsed = [math]::Round($timing.TotalSeconds)
+Write-Host `r`n"Download took $elapsed seconds"
