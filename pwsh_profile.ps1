0012260b81587b67426375a8dee23850ebdd28b3
sombal <sombal@gmail.com>
2021-03-12 21:21:38 +0100

    update psprofile

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -1,16 +1,31 @@
 #Start Transcript
-function Start-Log {
-    $logPath = "path-to-logfile"
-    $transcriptDate = Get-Date -Format yyyyMMdd_HHmmss
-    $transcriptPath = $logPath+"\"+$transcriptDate+".pslog"
-    Start-Transcript -Path $transcriptPath | Out-Null
-}
-#Start-Log
+$logPath = "$env:USERPROFILE\Documents\Powershell\Logs"
+$transcriptDate = Get-Date -Format yyyyMMdd
+$transcriptPath = $logPath+"\"+"pwsh_transcript_"+$transcriptDate+".log"
+Start-Transcript -Path $transcriptPath -Append | Out-Null
 
-#Change Prompt Text
+#Env setup
+$repoPath = "D:\OneDrive\Profiles\Private\Sources"
+$env:Path += ";C:\Program Files\Git\bin"
+Set-Location $repoPath
+Set-ExecutionPolicy -ExecutionPolicy ByPass -Scope Process
+
+#Set cmd line prediction
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+#Set oh my posh prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+
+Set-PoshPrompt -Theme material
+
+<#
+#Change Prompt Text (old way)
 function global:prompt {
-    $now = Get-Date -format "HH:mm:ss"
+    $now = Get-Date -format "HH:mm"
     Write-Host "[$now] " -NoNewLine -ForegroundColor DarkGray
+    Write-Host "$env:USERNAME@$($env:COMPUTERNAME.ToLower()) " -NoNewLine -ForegroundColor Cyan
     Write-Host "..\$($pwd.Path.Split("\")[-1]) >" -NoNewLine -ForegroundColor Green
     return " "
-}
+}#>
